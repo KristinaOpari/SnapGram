c6205b3e27c54efc35945ce409df40a29dea0fe9
kopari <kopari@deloittece.com>
2024-02-08 14:04:50 +0100

    Created create-post and initial hsowing logic of posts

--- src/lib/react-query/queriesAndMutation.ts
+++ src/lib/react-query/queriesAndMutation.ts
@@ -5,11 +5,14 @@ import {
     useInfiniteQuery,
 } from '@tanstack/react-query';
 import {
+    createPost,
     createUserAccount,
+    getRecentPosts,
     signInAccount,
     signOutAccount,
 } from '../appwrite/api';
-import { INewUser } from '@/types';
+import { INewPost, INewUser } from '@/types';
+import { QUERY_KEYS } from './queryKey';
 
 //Initialize mutation function so react query knows what we are doing, this can be utilized in the signup form to create the user account
 export const useCreateUserAccount = () => {
@@ -28,5 +31,24 @@ export const useSignInAccount = () => {
 export const useSignOutAccount = () => {
     return useMutation({
         mutationFn: () => signOutAccount(),
+    });
+};
+
+export const useCreatePost = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: (post: INewPost) => createPost(post),
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+            });
+        },
+    });
+};
+
+export const useGetRecentPosts = () => {
+    return useQuery({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+        queryFn: () => getRecentPosts(),
     });
 };
